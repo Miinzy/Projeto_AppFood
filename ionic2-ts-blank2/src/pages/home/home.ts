a66efdee1eb2ae41b3a766389039538e75782d57
Lorenna <Miinzy@users.noreply.github.com>
2017-07-21 14:03:55 -0300

    Projeto
    Home e Menu

---
+++ ionic2-ts-blank2/src/pages/home/home.ts
@@ -0,0 +1,29 @@
+﻿import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+  
+  onLink(url: string) {
+      window.open(url);
+  }
+}
+export class Home {
+    public toggled: boolean;
+
+    constructor() {
+        this.toggled = false;
+    }
+
+    toggleSearch() {
+        this.toggled = this.toggled ? false : true;
+    }
+}
